03029a2107bc4505ebbf43d5c446e00366371c87
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 14:45:27 -0400

    formatting for log writeline

---
+++ Code/Chapter02/formatting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfApples = 12;
+            decimal pricePerApple = 0.35M;
+            Console.WriteLine(
+                format: "{0} apples costs {1:C}",
+                arg0: numberOfApples,
+                arg1: (pricePerApple * numberOfApples)
+            );
+        }
+    }
+}
