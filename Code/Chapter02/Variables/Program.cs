535ce36aea05f6d98841cf40024b4280a41018c6
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 14:10:01 -0400

    using object type

--- Code/Chapter02/Variables/Program.cs
+++ Code/Chapter02/Variables/Program.cs
@@ -7,8 +7,11 @@ namespace Variables
         static void Main(string[] args)
         {
             object height = 1.88;
-            object name = "Amir";
+            object name = "DinoMike";
             Console.WriteLine($"{name} is {height} meters tall.");
+            // int length1 = name.Length;
+            int length2 = ((string)name).Length;
+            Console.WriteLine($"{name} has {length2} characters.");
         }
     }
 }
