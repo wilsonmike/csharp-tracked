daa511a8296008096c5463cfc4a6e3dbf9a51f74
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 13:27:07 -0400

    added variable folder

--- Code/Chapter02/numbers/Program.cs
+++ Code/Chapter02/numbers/Program.cs
@@ -16,6 +16,9 @@ namespace numbers
             double anotherRealNumber = 2.3;
 
             Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers");
+            Console.WriteLine($"uint uses {sizeof(uint)} bytes and can store numbers");
+            Console.WriteLine($"float uses {sizeof(float)} bytes and can store numbers");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers");
         }
     }
 }
