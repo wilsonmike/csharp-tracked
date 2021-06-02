2f804832915aee6c0ab38e1afa5ad54732fd9c0d
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 14:28:50 -0400

    iterating through name array

---
+++ Code/Chapter02/Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names;
+            names = new string[4];
+            names[0] ="Jack";
+            names[1] = "Jill";
+            names[2] = "Gingerbread";
+            names[3] = "Man";
+
+            for (int i = 0; i <names.Length; i++) {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
