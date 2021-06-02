01ab591672752136c14653dcc26cc656d960e431
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 12:21:50 -0400

    chapter 2 console app starting files

---
+++ Code/Chapter02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
