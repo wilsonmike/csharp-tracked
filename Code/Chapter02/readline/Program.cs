525d6a502b8c82579a649fa48771e84f721641b5
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 14:54:50 -0400

    storing user input

---
+++ Code/Chapter02/readline/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace readline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type your first name and press ENTER: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Type your age and press ENTER: ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"Hello {firstName}, you look great for {age}.");
+        }
+    }
+}
