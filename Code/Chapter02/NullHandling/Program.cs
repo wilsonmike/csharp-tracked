8c82bca3e9d8e1efcccba34a86bded56482f48e0
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 14:33:39 -0400

    working with null values

---
+++ Code/Chapter02/NullHandling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NullHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thisCannotBeNull = 4;
+            // thisCannotBeNull = null;
+            int? thisCouldBeNull = null;
+            thisCouldBeNull = 7;
+            Console.WriteLine(thisCouldBeNull);
+        }
+    }
+}
