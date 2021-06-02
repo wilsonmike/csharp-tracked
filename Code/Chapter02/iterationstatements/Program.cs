a35fa14fe39f2c6351a2ac7c24ffbbadbc843bdf
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 15:56:50 -0400

    while loop

---
+++ Code/Chapter02/iterationstatements/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace iterationstatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
