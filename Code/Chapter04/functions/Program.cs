0ce998b227bab9a2562158fc1708d3360d88303e
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-02 17:00:35 -0400

    eod commit pg 207

--- Code/Chapter04/functions/Program.cs
+++ Code/Chapter04/functions/Program.cs
@@ -4,6 +4,15 @@ namespace functions
 {
     class Program
     {
+        static void TimesTable(byte number)
+        {
+            Console.WriteLine($"This is the {number} times table:");
+            for (int row = 1; row <= 12; row++)
+            {
+                Console.WriteLine($"{row} x {number} = {row * number}");
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
